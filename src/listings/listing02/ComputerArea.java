ff92addb063508a9bf65449be31e121882e895f6
malitass4144 <malitass4144@gmail.com>
2022-08-23 16:03:43 +0300

    listings folder with a new listing

---
+++ src/listings/listing02/ComputerArea.java
@@ -0,0 +1,20 @@
+package listings.listing02;
+
+public class ComputerArea {
+	public static void main(String[] args) {
+		
+
+
+		double radius = 20 ;
+	    double area = radius*radius*3.14159 ;
+	    
+	    System.out.println(" The area for the circle of radius " + radius + " is " + area );
+	    
+	    
+	    
+	    
+	}
+
+	}
+
+
