8e2999d10a1c9ee5c63030cb7d9f3c945aa7639e
malitass4144 <malitass4144@gmail.com>
2022-08-23 16:41:01 +0300

    added listing 2.2

---
+++ src/listings/listing02/ComputerAreaWithConsoleInput.java
@@ -0,0 +1,23 @@
+package listings.listing02;
+
+import java.util.Scanner;
+
+public class ComputerAreaWithConsoleInput {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner (System.in);
+		
+		System.out.println("Enter a number for radius: ");
+	
+		double radius = input.nextDouble();
+		
+		double area = radius*radius*3.14159 ;
+		
+		System.out.println("The area for the circle of radius " + radius + " is " + area);
+		
+		
+
+	}
+
+}
